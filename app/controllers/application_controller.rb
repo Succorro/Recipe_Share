a4c44b261d617cb1ab7f2a9fb624c933330b6a54
Steven Gutierrez <stevogbmv@gmail.com>
2023-09-24 19:36:29 -0700

    Rails setup, installed active storage, image_processing, activemodel_serializers, config platform for ruby, installed bcrypt, byebug, listen, spring, ensured db is postgresql. Created user model, configured cookies "

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    # before_action :authorize 
+  
+    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+    # rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response 
+  
+    def hello_world
+        session[:count] = (session[:count] || 0) + 1
+        render json: { count: session[:count] }
+    end
+
+    private 
+  
+    # def authorize 
+    #   @current_user = User.find_by(id: session[:user_id])
+  
+    #   render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    # end
+  
+    # def render_not_found_response(exception)
+    #   render json: {error: "#{exception.message}"}, status: :not_found
+    # end
+  
+    # def render_unproccessable_entity_response(exception)
+    #     render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    # end
+end
