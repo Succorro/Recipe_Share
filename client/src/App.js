81e55b57673a5b603bb59321f04228f102433c93
Steven Gutierrez <stevogbmv@gmail.com>
2023-09-27 17:45:04 -0700

    Created react pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,50 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Recipes from "./pages/Recipes";
+import Recipe from "./pages/Recipe";
+import NewRecipe from "./pages/NewRecipe";
+import User from "./pages/User";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/users/profile">
+            <Profile />
+          </Route>
+          <Route path="/users/:id">
+            <User />
+          </Route>
+          <Route path="/recipes/new">
+            <NewRecipe />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/recipes">
+            <Recipes />
+          </Route>
+          <Route path="/about">
+            <About />
           </Route>
           <Route path="/">
-            <h1>Page Count: {count}</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ client/src/pages/NewRecipe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewRecipe() {
+  return <div>NewRecipe</div>;
+}
+
+export default NewRecipe;
